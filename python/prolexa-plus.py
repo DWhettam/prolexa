268a11bd4bb4cf1846767f37dcc206d0595e367f
Gavin Leech <gleech@protonmail.com>
2020-05-22 18:45:25 +0100

    more data, fixed data, cli template

--- python/prolexa-plus.py
+++ python/prolexa-plus.py
@@ -3,21 +3,18 @@
 
 from argparse import ArgumentParser
 from pyswip import Prolog
+from prolexa_pyswip import escape_and_call_prolexa
 
 swipl = Prolog()
-swipl.consult("prolog/prolexa.pl")
+swipl.consult("../prolog/prolexa.pl")
 
-STATIONS = [ans["S"] for ans in swipl.query("station(S)")]
 
-def route_length(From, To):
-    return next(swipl.query(f"route_length({From}, {To}, L)"))["L"]
 
 def main():
-    parser = ArgumentParser(description="Find the length of a route between two stations")
-    parser.add_argument("-o", "--origin", choices=STATIONS, required=True)
-    parser.add_argument("-d", "--destination", choices=STATIONS, required=True)
+    parser = ArgumentParser(description="Hello! I'm ProlexaPlus! Tell me anything, ask me anything.")
+    parser.add_argument("-o", "--input", required=True)
     args = parser.parse_args()
-    print(route_length(args.origin, args.destination))
+    print(escape_and_call_prolexa(args.input))
 
 if __name__ == "__main__":
     main()
